4e1ace9bd87d461f0c1d38a294fba2afd7cc930c
dileep kumar <152362477+Dileepkumar-web@users.noreply.github.com>
2025-06-30 21:18:42 +0530

    Add files via upload

---
+++ nextpermutation.java
@@ -0,0 +1,38 @@
+public class Solution {
+    public void nextPermutation(int[] nums) {
+        int n = nums.length;
+        int i = n - 2;
+
+        // Step 1: Find first decreasing element from end
+        while (i >= 0 && nums[i] >= nums[i + 1]) {
+            i--;
+        }
+
+        if (i >= 0) {
+            // Step 2: Find element just larger than nums[i]
+            int j = n - 1;
+            while (nums[j] <= nums[i]) {
+                j--;
+            }
+            // Step 3: Swap
+            swap(nums, i, j);
+        }
+
+        // Step 4: Reverse the suffix starting at i + 1
+        reverse(nums, i + 1, n - 1);
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    private void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            swap(nums, start, end);
+            start++;
+            end--;
+        }
+    }
+}
